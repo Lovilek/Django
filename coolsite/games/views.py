0fdb76831219fdc100657b9565d4f8ee7b9b1735
Lovilek <wingeddemon2274@gmail.com>
2023-02-12 16:36:12 +0600

    ...

--- coolsite/games/views.py
+++ coolsite/games/views.py
@@ -1,20 +1,58 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404,HttpResponseForbidden,HttpResponseBadRequest,HttpResponseServerError
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu=[
+    {'title':"О сайте",'url_name':'about'},
+    {'title':"Добавить статью",'url_name':'add_page'},
+    {'title':"Обратная связь",'url_name':'contact'},
+    {'title':"Войти",'url_name':'login'}
+
+]
 
 def index(request):
-    return HttpResponse("Games")
+    posts=Games.objects.all()
+    cats=Category.objects.all()
 
-def categories(request,carid):
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Cars</h1><p> {carid}</p>")
+    context={
+        'cats':cats,
+        'posts':posts,
+        'menu':menu,
+        'title': 'Главная страница',
+        'cat_selected':0,
+    }
+    return render(request, 'games/index.html',context=context)
 
-def archive(request,year):
-    if int(year)>2023:
-        return redirect('home', permanent=False)
+def about(request):
+    return render(request, 'games/about.html',{'title': 'About','menu':menu})
 
-    return HttpResponse(f"<h1>Архив по годам </h1><p>{year}</p>")
+def addpage(request):
+    return HttpResponse("Добавление игры")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def show_post(request,post_id):
+    return HttpResponse(f"Отображение статьи с id={post_id}")
+
+def show_category(request,cat_id):
+    posts = Games.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts)==0:
+        raise Http404()
+
+    context = {
+        'cats': cats,
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'games/index.html', context=context)
+def login(request):
+    return HttpResponse("Авторизация")
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
