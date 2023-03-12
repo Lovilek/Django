f26c30d41664343d8ca4e6c4ee35539348ac72fe
Lovilek <wingeddemon2274@gmail.com>
2023-03-12 20:01:18 +0600

    ...

--- coolsite/games/views.py
+++ coolsite/games/views.py
@@ -28,13 +28,11 @@ def about(request):
 
 def addpage(request):
     if request.method=='POST':
-        form=AddPostForm(request.POST)
+        form=AddPostForm(request.POST,request.FILES)
         if form.is_valid():
-            try:
-                Games.objects.create(**form.cleaned_data)
-                return  redirect('home')
-            except:
-                form.add_error(None,'Ошибка')
+            form.save()
+            return  redirect('home')
+
     else:
         form=AddPostForm()
     return render(request,'games/addpage.html',{'form':form,'menu':menu,'title':'Добавление статьи'})
